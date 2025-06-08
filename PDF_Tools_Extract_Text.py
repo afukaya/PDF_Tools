4a91db7d3742498aaa18002dab188cdc439a0e8c
Alexandre Fukaya <afukaya@hotmail.com>
2025-06-08 00:13:38 -0300

    first commit

---
+++ PDF_Tools_Extract_Text.py
@@ -0,0 +1,24 @@
+#
+# Extracts text from a PDF File.
+# By ChatGPT.
+
+import PyPDF2
+
+def extrair_texto_pdf(caminho_arquivo):
+    # Abrindo o arquivo PDF em modo binário
+    with open(caminho_arquivo, 'rb') as arquivo:
+        leitor_pdf = PyPDF2.PdfReader(arquivo)
+        
+        # Inicializando uma string para armazenar todo o texto
+        texto_completo = ''
+        
+        # Iterando sobre cada página do PDF
+        for numero_pagina in range(len(leitor_pdf.pages)):
+            pagina = leitor_pdf.pages[numero_pagina]
+            print('Pagina: ')
+            print(pagina.extract_text())
+            print('\n--------------------------------\n')
+
+# Exemplo de uso
+caminho_do_pdf = 'C:\\Users\\2009303\\Downloads\\DoD_DevSecOps_Fundamentals_Guidebook.pdf'
+extrair_texto_pdf(caminho_do_pdf)
